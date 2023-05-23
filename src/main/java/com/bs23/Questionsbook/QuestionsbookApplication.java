98b8343b29ece5694894a6281cef6974de1b5db6
Tonmoy-Saha-BS23 <tonmoy.saha@brainstation-23.com>
2023-05-23 18:27:02 +0600

    project set up is ready

---
+++ src/main/java/com/bs23/Questionsbook/QuestionsbookApplication.java
@@ -0,0 +1,13 @@
+package com.bs23.Questionsbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bs23/Questionsbook/QuestionsbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs23.Questionsbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionsbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
